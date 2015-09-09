2b18fcfa6e3ba7af21c07cb20b742a96e7292436
Jim O'Regan <joregan@gmail.com>
2015-09-09 20:30:30 +0100

    also note where index.sense comes from

--- gawncvt.pl
+++ gawncvt.pl
@@ -17,6 +17,7 @@ my $skipline = 0;
 
 open(LEMON, '>>gawn-lemon.ttl') or die "$!\n";
 open(VUWN, '>>gawn-vu.ttl') or die "$!\n";
+# index.sense from http://wordnetcode.princeton.edu/3.0/WNdb-3.0.tar.gz
 open(SENSES, '<index.sense') or die "$!\n";
 
 my %posmap = (
