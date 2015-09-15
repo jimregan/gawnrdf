24f1560546ad03999e2ad80e9ece99f09444c7ce
Jim O'Regan <joregan@gmail.com>
2015-09-15 12:06:54 +0100

    collect affixes from Princeton WordNet

---
+++ collect-affix-simple.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+use utf8;
+
+my %seen = ();
+
+my $CNS = '[bcdfghjklmnpqrstvwxyz]';
+
+my %str = ();
+
+while (<>) {
+	chomp;
+	next unless (/wn30:wordsense/);
+	my @parts = split / /, $_;
+	my $left = '';
+	my $right = '';
+	if ($parts[0] =~ /wn30:wordsense\-(.*)-(noun|verb|adjective|adverb)-([0-9]*)/) {
+		$left = $1;
+	} else {
+		print STDERR "Line $. does not match: $_\n";
+	}
+
+	if ($parts[2] =~ /wn30:wordsense\-(.*)-(noun|verb|adjective|adverb)-([0-9]*)/) {
+		$right = $1;
+	} else {
+		print STDERR "Line $. does not match: $_\n";
+	}
+
+	if ($right =~ /$left/) {
+		my $aff = $right;
+		$aff =~ s/$left//;
+		next if ($aff eq '');
+		my $lastleft = '';
+		if ($left =~ /($CNS)$/) {
+			$lastleft = $1;
+		}
+		if ($aff =~ /^$lastleft/ && ($right eq ($left . $aff))) {
+			$aff =~ s/^$lastleft//;
+		}
+		if (!exists $seen{$parts[1] . $aff}) {
+			$str{$parts[1] . $aff} = "$parts[1]: $aff\t$left\t$right";
+			$seen{$parts[1] . $aff} = 1;
+		} else {
+			$seen{$parts[1] . $aff}++;
+			if ($aff eq 'fe' || $aff eq 't') {
+				print STDERR "f:: $left $right\n";
+			}
+		}
+	}
+}
+
+foreach my $v (keys %seen) {
+	print $seen{$v} . "\t" . $str{$v} . "\n";
+}
