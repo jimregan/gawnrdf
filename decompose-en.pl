3a29a8485b881247339a30555af3c076978e66e2
Jim O'Regan <joregan@gmail.com>
2015-09-15 13:16:20 +0100

    generated suffixes

--- decompose-en.pl
+++ decompose-en.pl
@@ -37,6 +37,19 @@ my %prefixes = (
 my %suffixes = (
 	'adjectivePertainsTo' => ['ical', 'ish', 'ural', 'ic', 'ine', 'ate', 'ly', 'an', 'n', 'nical', 'atic', 'ine'],
 	'adverbPertainsTo' => ['ly', 'erly', 'y', 'ally'],
+	"wnschema:agent" => ["ate", "ation", "e", "ee", "eer", "er", "ion", "ior", "ise", "ize", "ker", "or", "r", "yer"],
+	"wnschema:body-part" => ["ate", "er", "or", "r"],
+	"wnschema:by_means_of" => ["ance", "ate", "ation", "ence", "er", "fy", "ify", "ion", "ise", "ition", "ize", "ment", "or", "r"],
+	"wnschema:destination" => ["e", "ee", "ify"],
+	"wnschema:event" => ["al", "ament", "ance", "ate", "ation", "eate", "ee", "ence", "ent", "er", "ify", "ing", "ion", "isation", "ise", "ition", "ization", "ize", "ment", "nce", "or", "r", "sion"],
+	"wnschema:instrument" => ["ator", "er", "ise", "ize", "ment", "or", "r"],
+	"wnschema:location" => ["alise", "alize", "er", "ize", "ment", "r"],
+	"wnschema:material" => ["ate", "eate", "er", "ify", "ise", "ize", "or", "r"],
+	"wnschema:property" => ["ance", "ate", "ation", "ence", "ion", "ment", "nce"],
+	"wnschema:result" => ["al", "alize", "ance", "ate", "ation", "der", "er", "ify", "ion", "ise", "ize", "ment", "nce", "r", "sion"],
+	"wnschema:state" => ["ance", "ation", "ence", "ion", "ise", "ize", "ment", "nce", "or"],
+	"wnschema:undergoer" => ["al", "ance", "ation", "e", "ee", "ence", "er", "ify", "ion", "ize", "ment", "nce", "r"],
+	"wnschema:uses" => ["alize", "ament", "ance", "ate", "ation", "ify", "ing", "ise", "ize", "ment"],
 );
 
 my %affixsense = (
